6f0d96268ba1199b87e0775a1c4a16b1e56d409e
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-03 10:58:24 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join("{}" .format(chr(char)) for char in range(ord('a'), ord('z') + 1)), end="")
+print("".join("{}" .format(chr(char))
+        for char in range(ord('a'), ord('z') + 1)), end="")
