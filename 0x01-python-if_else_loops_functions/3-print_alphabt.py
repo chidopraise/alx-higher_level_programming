a2791e4f6132314245841d607299b337824e69ad
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-03 11:09:35 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(f"{chr(char)}" for char in range(ord('a'), ord('z') + 1) if chr(char) not in 'qe'), end="")
+print("".join("{}".format(chr(char)) for char in range(ord('a'), ord('z') + 1) if chr(char) not in 'qe'), end="")
