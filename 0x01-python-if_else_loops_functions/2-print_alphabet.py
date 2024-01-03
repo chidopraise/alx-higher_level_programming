2951a4d835232b2d686f460bc9e11aa5dc60c928
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-03 10:40:39 -0800

    2-print_alphabet.py2-print_alphabet.p

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end="")
-print()  # Adding a new line at the end for better formatting
+print("".join(chr(char) for char in range(ord('a'), ord('z') + 1)))
 
