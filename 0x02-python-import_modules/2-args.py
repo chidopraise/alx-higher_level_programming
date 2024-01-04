24ca0bf1f2c6b0e03d1fe8aa5e7795ef5ccdd57f
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-04 06:25:19 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} {}:".format(argc, "argument" if argc == 1 else "arguments"))
+
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
