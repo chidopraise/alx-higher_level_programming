05e41a9d25d9c2dfc10494d148de7e7bd8bc17e6
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-04 07:18:39 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
     c = 0
 
     if a < b:
+        from magic_calculation_102 import add
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
     else:
+        from magic_calculation_102 import sub
         c = sub(a, b)
 
     return c
