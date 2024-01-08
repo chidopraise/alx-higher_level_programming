deee252993e689c01527e529f88f1d264515a780
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-08 07:03:53 -0800

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     # Delete the item at a specific position in the list
-    if idx < 0 or idx >= len(my_list):
-        # If idx is negative or out of range, return the same list
-        return my_list
-
-    # Create a new list with the element at the specified index removed
-    return my_list[:idx] + my_list[idx + 1:]
+    if 0 <= idx < len(my_list):
+        # If idx is within the valid range, delete the element at idx
+        del my_list[idx]
+    return my_list
