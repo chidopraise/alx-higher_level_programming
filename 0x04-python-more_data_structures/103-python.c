c4f1572f5bf601bc88b803d6f23b0429cda2485e
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-10 12:10:52 -0800

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,4 @@
 #include <Python.h>
-#include <stdio.h>
-#include <string.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
@@ -14,12 +12,6 @@ void print_python_list(PyObject *p)
 	Py_ssize_t size, i;
 	PyObject *element;
 
-	if (!PyList_Check(p))
-	{
-		fprintf(stderr, "[ERROR] Invalid Python List\n");
-		return;
-	}
-
 	size = PyList_Size(p);
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
@@ -28,12 +20,6 @@ void print_python_list(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		element = PyList_GetItem(p, i);
-		if (element == NULL)
-		{
-			fprintf(stderr, "[ERROR] Element retrieval failed\n");
-			return;
-		}
-
 		printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
 		if (PyBytes_Check(element))
 			print_python_bytes(element);
@@ -49,23 +35,11 @@ void print_python_bytes(PyObject *p)
 	Py_ssize_t size, i;
 	char *string;
 
-	if (!PyBytes_Check(p))
-	{
-		fprintf(stderr, "[ERROR] Invalid Bytes Object\n");
-		return;
-	}
-
 	printf("[.] bytes object info\n");
 	size = PyBytes_Size(p);
 	printf("  size: %ld\n", size);
 
 	string = PyBytes_AsString(p);
-	if (string == NULL)
-	{
-		fprintf(stderr, "[ERROR] Unable to retrieve string data\n");
-		return;
-	}
-
 	printf("  trying string: %s\n", string);
 
 	printf("  first 10 bytes: ");
