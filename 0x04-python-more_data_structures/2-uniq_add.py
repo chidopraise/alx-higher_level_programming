19f186d4d325a8823e26bb959c9ed485a1638313
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-10 06:36:10 -0800

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    # Use set to keep track of unique integers
+    unique_set = set()
+
+    # Iterate through the list and add unique integers to the set
+    for num in my_list:
+        unique_set.add(num)
+
+    # Sum up the unique integers in the set
+    result = sum(unique_set)
+
+    return result
