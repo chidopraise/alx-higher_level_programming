4631b8ee94e71bcd62b60b9bc7252963a7f46f4f
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-10 07:26:48 -0800

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # Update the value if the key exists, otherwise add a new key-value pair
+    a_dictionary[key] = value
+    return a_dictionary
