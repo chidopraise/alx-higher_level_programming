5c236d3fe6cf77c7fa22ca512a437ea438b2cd19
Abraham Livinus <abrahamlivinus@gmail.com>
2024-01-23 11:49:00 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Class that defines a square.
+"""
+
+
+class Square:
+    """
+    Class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
+        self.__size = size
